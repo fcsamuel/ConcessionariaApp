d01d0184c60c3df7265f27a15eb2c34f7316b69b
MatheusSantosScuzziatto <matheusscuzzi@gmail.com>
2019-06-07 20:21:18 -0300

    Components criados

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VeiculoComponent } from './cadastros/veiculo/veiculo.component';
+import { MarcaComponent } from './cadastros/marca/marca.component';
+import { ClienteComponent } from './cadastros/cliente/cliente.component';
+import { LocacaoComponent } from './cadastros/locacao/locacao.component';
+import { ClienteListComponent } from './cadastros/cliente/cliente-list/cliente-list.component';
+import { LocacaoListComponent } from './cadastros/locacao/locacao-list/locacao-list.component';
+import { MarcaListComponent } from './cadastros/marca/marca-list/marca-list.component';
+import { VeiculoListComponent } from './cadastros/veiculo/veiculo-list/veiculo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VeiculoComponent,
+    MarcaComponent,
+    ClienteComponent,
+    LocacaoComponent,
+    ClienteListComponent,
+    LocacaoListComponent,
+    MarcaListComponent,
+    VeiculoListComponent
   ],
   imports: [
     BrowserModule,
