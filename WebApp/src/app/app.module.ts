bf3fdf463b03d0b591d70ee1937fd23347f52da8
Felipe <felipecarvalhosamuel@gmail.com>
2019-06-07 21:52:07 -0300

    injectable do base, imports do module, ngx-spinner

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ANALYZE_FOR_ENTRY_COMPONENTS } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +13,14 @@ import { LocacaoListComponent } from './cadastros/locacao/locacao-list/locacao-l
 import { MarcaListComponent } from './cadastros/marca/marca-list/marca-list.component';
 import { VeiculoListComponent } from './cadastros/veiculo/veiculo-list/veiculo-list.component';
 import { DialogComponent } from './shared/dialog/dialog.component';
+import { HttpClient } from 'selenium-webdriver/http';
+import { DatePipe } from '@angular/common';
+
+import { MatInputModule, MatRadioModule, MatButtonModule, MatTableModule, MatIconModule,
+  MatPaginatorModule, MatSort, MatSortModule, MatExpansionModule, MatSelectModule, MatDialogModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner'; 
 
 @NgModule({
   declarations: [
@@ -30,9 +38,25 @@ import { DialogComponent } from './shared/dialog/dialog.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule, 
+    MatTableModule, 
+    MatIconModule, 
+    HttpClientModule,
+    MatPaginatorModule, 
+    MatSortModule, 
+    MatExpansionModule, 
+    MatSelectModule,
+    FormsModule,
+    MatDialogModule,
+    NgxSpinnerModule
   ],
-  providers: [],
+  providers: [HttpClient, DatePipe],
+  entryComponents: [ DialogComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
